5981124c96fe2d69c4945baed8366d63f6bc80e5
Den_Varenik <varenik.denisua@gmail.com>
2021-09-09 16:53:23 +0300

    Create Like

--- posts/api/permissions.py
+++ posts/api/permissions.py
@@ -8,3 +8,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
         else:
             return obj.author == request.user or request.user.is_staff
+
+
+class IsLikerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj.like_user == request.user or request.user.is_staff
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.validators import ValidationError
 
-from posts.models import Post
-from posts.api.serializers import PostSerializer
-from posts.api.permissions import IsAuthorOrReadOnly
+from posts.models import Post, Like
+from posts.api.serializers import PostSerializer, LikeSerializer
+from posts.api.permissions import IsAuthorOrReadOnly, IsLikerOrReadOnly
 
 
 class PostVS(viewsets.ModelViewSet):
@@ -13,3 +15,30 @@ class PostVS(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         author = self.request.user
         serializer.save(author=author)
+
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        pk = self.kwargs["pk"]
+        return Like.objects.filter(like_post=pk)
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        like_post = Post.objects.get(pk=pk)
+
+        like_user = self.request.user
+        like_queryset = Like.objects.filter(like_post=like_post, like_user=like_user)
+
+        if like_queryset.exists():
+            raise ValidationError("You have already liked this post!")
+
+        serializer.save(like_post=like_post, like_user=like_user)
+
+
+class LikeDelete(generics.DestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsLikerOrReadOnly]
